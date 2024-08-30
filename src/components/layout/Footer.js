8dd746fdd2793283ccb8d3c965f415ba82381dea
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-30 18:20:34 +0330

    create footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+
+import { MdOutlineRealEstateAgent } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className='flex  font-Dana items-center p-2 mx-auto w-full bg-violet-600'>
+      <p className='font-DanaDemiBold text-white w-full content-center p-1 m-1 text-center   '>
+        آلفا | حسن اشرفی
+      </p>
+    </div>
+
+  )
+}
+
+export default Footer
