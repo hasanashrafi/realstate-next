e0c901d2631e1ec07357f941f237fc3070475f91
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-30 18:21:01 +0330

    added  header footer components

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
-import Image from "next/image";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       <p>Real State</p>
+    <main className="flex min-h-screen flex-col  mx-auto w-full ">
+      <Header/>
+      <div className=" max-w-5xl my-5 rounded-md mx-auto h-screen w-full  bg-blue-200 font-mono text-sm ">
+       <p className="font-DanaDemiBold text-3xl text-violet-800 p-5">املاک آلفا</p>
       </div>
+      <Footer/>
     </main>
   );
 }
