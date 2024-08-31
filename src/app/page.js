5dce811e7d49fd77966b1888d5ea5c82b0cdad09
Hasan Ashrafi <yasashrafi@gmail.com>
2024-08-31 18:48:45 +0330

    remove header and footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  mx-auto w-full ">
-      <Header/>
-      <div className=" max-w-5xl my-5 rounded-md mx-auto h-screen w-full  bg-blue-200 font-mono text-sm ">
+    <main className="flex min-h-screen flex-col  mx-auto w-full p-4">
+     
+      <div className=" max-w-5xl  rounded-md mx-auto h-screen w-full  bg-blue-200 font-mono text-sm shadow-dark">
        <p className="font-DanaDemiBold text-3xl text-violet-800 p-5">املاک آلفا</p>
       </div>
-      <Footer/>
+      
     </main>
   );
 }
