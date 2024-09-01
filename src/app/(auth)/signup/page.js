16000d3458029ae51f95af04d62530604a896f68
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-01 17:55:43 +0330

    added get session for authorization

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,7 +1,13 @@
 import SignupPage from '@/components/template/SignupPage'
-import React from 'react'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-function SignUp() {
+
+async function SignUp() {
+  const session = await getServerSession(authOptions)
+  if(session) redirect("/")
+    
   return <SignupPage/>
 }
 
