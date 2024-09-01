bf407e84dd7a7b29eb51719821bee1a74d53ac7a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-01 14:26:46 +0330

    wrap next auth provider around app

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import Layout from "@/components/layout/Layout";
 import "./globals.css";
+import NexAuthProvider from '@/providers/NextAuthProvider';
 
 
 
@@ -13,9 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className=""><Layout>
-      {children}
-      </Layout>
+      <body className="">
+        <NexAuthProvider>
+          <Layout>
+            {children}
+          </Layout>
+        </NexAuthProvider>
       </body>
     </html>
   );
