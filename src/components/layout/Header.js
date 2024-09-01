a2134ab190afb52d91bf39058fbf8e2b2fbd9aaa
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-01 17:56:27 +0330

    get data with useSession

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,14 @@
+"use client"
+
 import React from 'react'
-import { FaRegUser } from "react-icons/fa";
-import { MdOutlineHomeWork } from "react-icons/md";import Link from 'next/link';
+import { LuLogIn } from "react-icons/lu";import { MdOutlineHomeWork } from "react-icons/md"; import Link from 'next/link';
+import { useSession } from 'next-auth/react';
+import { LuLayoutDashboard } from "react-icons/lu";
+
 
 function Header() {
+    const { data } = useSession()
+    console.log(data)
     return (
         <header className='flex justify-between font-Dana items-center px-5 p-4 mx-auto w-full bg-violet-600'>
             <div className='flex p-1 items-center text-white'>
@@ -15,10 +21,21 @@ function Header() {
                     </li>
                 </ul>
             </div>
-            <p className='flex gap-2  font-DanaDemiBold text-white '><MdOutlineHomeWork  className='text-4xl font-bold' /></p>
-            <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
-                <Link href="/signup" className=' '> <FaRegUser /> </Link>
-            </div>
+            <p className='flex gap-2  font-DanaDemiBold text-white '><MdOutlineHomeWork className='text-4xl font-bold' /></p>
+            {
+                data ? (
+                    <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
+                        <Link href="/dashboard" className=' '> <LuLayoutDashboard />
+                        </Link>
+                    </div>
+                ) : (
+
+                    <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
+                        <Link href="/signup" className=' '> <LuLogIn />
+                        </Link>
+                    </div>
+                )
+            }
         </header>
     )
 }
