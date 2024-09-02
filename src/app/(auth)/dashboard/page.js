58e13697900e104af379efd9d5f279196099fc8f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-02 23:08:15 +0330

    create dashboard route and get user data with session

---
+++ src/app/(auth)/dashboard/page.js
@@ -0,0 +1,16 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import DashboardPage from "@/components/template/DashboardPage"
+import User from "@/models/User"
+import connectDB from "@/utils/connectDB"
+import { getServerSession } from "next-auth"
+
+
+async function Dashboard() {
+  await connectDB()
+  const session = await getServerSession(authOptions)
+  const user = await User.findOne({ email: session.user.email })
+
+  return <DashboardPage createdAt={user.createdAt}/>
+}
+
+export default Dashboard
