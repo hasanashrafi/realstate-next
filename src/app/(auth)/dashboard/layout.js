3bb234e29c3cea2c42a404f84680c6c57f9a7e1e
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-02 23:07:45 +0330

    create dashboard layout and get user data with session

---
+++ src/app/(auth)/dashboard/layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import DashboardSideBar from '@/components/layout/DashboardSideBar'
+
+async function DashboardLayout({ children }) {
+    const session = await getServerSession(authOptions)
+    if (!session) redirect("/signin")
+
+    return (
+        <DashboardSideBar className='min-h-screen bg-red-500 '>
+            {children}
+        </DashboardSideBar>
+    )
+}
+
+export default DashboardLayout
