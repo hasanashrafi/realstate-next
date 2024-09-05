12a6892659b2c57b107bea1fb8eb4d46c6b60df7
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-05 10:29:06 +0330

    create reusable text inputs

---
+++ src/components/module/TextInput.js
@@ -0,0 +1,48 @@
+import { p2e } from '@/utils/replaceNumbers'
+import React from 'react'
+
+function TextInput({
+    title,
+    name,
+    profileData,
+    setProfileData,
+    textarea = false
+}) {
+
+    const changeHandler = (e) => {
+        const { name, value } = e.target
+        setProfileData({ ...profileData, [name]: p2e(value) })
+    }
+
+    return (
+        <div className=''>
+            <p>{title}</p>
+            {
+                textarea ?
+                    (
+                        <textarea
+                            onChange={changeHandler}
+                            name={name}
+                            value={profileData[name]}
+                            type='text'
+                            placeholder='...'
+                            className='w-80 h-36 p-2 my-1 text-justify rounded-md border focus:border-blue-600 outline-none'
+                        />
+
+                    ) : (
+                        <input
+                            onChange={changeHandler}
+                            name={name}
+                            value={profileData[name]}
+                            type='text'
+                            placeholder='عنوان را وارد کنید  ...'
+                            className='p-2 w-80 my-1 focus:border focus:border-blue-600 rounded-md border outline-none' />
+                    )
+            }
+
+
+        </div>
+    )
+}
+
+export default TextInput
