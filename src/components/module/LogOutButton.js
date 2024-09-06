cea9a2c3eb2fdd98671dac8f2a32abc0d0f05a83
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-06 11:32:02 +0330

    edit logout button style

--- src/components/module/LogOutButton.js
+++ src/components/module/LogOutButton.js
@@ -4,9 +4,9 @@ import { FiLogOut } from "react-icons/fi";
 function LogOutButton() {
     return (
         <button onClick={signOut}
-        className="flex justify-center gap-x-3 items-center font-DanaDemiBold w-fit text-center  rounded-lg p-2 mt-2 bg-red-500 hover:bg-red-700 text-white ">
+        className="flex justify-center gap-x-1 items-center text-sm font-DanaDemiBold w-fit text-center  rounded-lg p-2 mt-2 bg-red-500 hover:bg-red-700 text-white ">
             <FiLogOut  className=""/>
-            <span className="text-[12px] ">خروج از حساب کاربری</span>
+            <span className="hidden md:inline-block w-fit text-[12px] ">خروج از حساب کاربری</span>
         </button>
     )
 }
