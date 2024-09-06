2140073f125443c94384a66754b44c81fdeeb6a2
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-06 19:01:11 +0330

    edit styles

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,44 +1,73 @@
 "use client"
 
-import React from 'react'
-import { LuLogIn } from "react-icons/lu";import { MdOutlineHomeWork } from "react-icons/md"; import Link from 'next/link';
+import React, { useState } from 'react'
+import { LuLogIn } from "react-icons/lu"; import { MdOutlineHomeWork } from "react-icons/md"; import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { LuLayoutDashboard } from "react-icons/lu";
+import { IoMenu } from "react-icons/io5";
 
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
     const { data } = useSession()
     console.log(data)
-    return (
-        <header className='flex justify-between font-Dana items-center px-5 p-2 mx-auto w-full bg-violet-600'>
-            <div className='flex  items-center text-white'>
-                <ul className='flex  justify-start text-sm'>
-                    <li className='p-1'>
-                        <Link href="/">صفحه اصلی</Link>
-                    </li>
-                    <li className='p-1'>
-                        <Link href="/buy-residential">آگهی ها </Link>
-                    </li>
-                </ul>
-            </div>
-           
-            <MdOutlineHomeWork className='text-3xl text-white font-bold' />
-            {
-                data ? (
-                    <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
-                        <Link href="/dashboard" className=' '> <LuLayoutDashboard />
-                        </Link>
-                    </div>
-                ) : (
 
-                    <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
-                        <Link href="/signup" className=' '> <LuLogIn />
-                        </Link>
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    }
+
+return (
+    <header className='flex justify-between font-Dana items-center px-5 p-2 mx-auto w-full bg-violet-600'>
+        <div className='flex items-center text-white'>
+            <div className='relative sm:hidden'>
+                <button
+                    onClick={toggleDropdown}
+                    className=' p-2 bg-blue-500 rounded '
+                >
+                    <IoMenu />
+                </button>
+                {isOpen && (
+                    <ul className=' w-24 p-1 absolute  bg-gray-500 text-sm mt-2 rounded shadow-lg'>
+                        <li className='p-1'>
+                            <Link href="/">صفحه اصلی</Link>
+                        </li>
+                        <li className='p-1'>
+                            <Link href="/buy-residential">آگهی ها</Link>
+                        </li>
+                    </ul>
+                )}
+            </div>
+
+            <div> 
+            <ul className='hidden sm:flex p-1 text-sm '>
+                        <li className='p-1'>
+                            <Link href="/">صفحه اصلی</Link>
+                        </li>
+                        <li className='p-1'>
+                            <Link href="/buy-residential">آگهی ها</Link>
+                        </li>
+                    </ul>
                     </div>
-                )
-            }
-        </header>
-    )
+        </div>
+
+        <MdOutlineHomeWork className='text-3xl text-white font-bold' />
+        {
+            data ? (
+                <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
+                    <Link href="/dashboard" className=' '> <LuLayoutDashboard />
+                    </Link>
+                </div>
+            ) : (
+
+                <div className=' text-white text-2xl hover:text-indigo-200 transition-all ease-in-out'>
+                    <Link href="/signup" className=' '> <LuLogIn />
+                    </Link>
+                </div>
+            )
+        }
+    </header>
+)
 }
 
 export default Header
