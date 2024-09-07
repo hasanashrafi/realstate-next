9603c6cc6b624fe08f68526a693250f35faaf7f2
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-07 22:15:52 +0330

    edit styles

--- src/components/layout/DashboardSideBar.js
+++ src/components/layout/DashboardSideBar.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { getServerSession } from 'next-auth'
-import { CgProfile } from "react-icons/cg"
 
+import { CgProfile } from "react-icons/cg"
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import LogOutButton from '../module/LogOutButton'
 
@@ -10,28 +10,30 @@ async function DashboardSideBar({ children }) {
     const session = await getServerSession(authOptions)
 
     return (
-        <div className='w-[95%]  p-2 mx-auto flex flex-col md:flex-row md:items-start md:gap-x-3 justify-center items-center gap-y-5  mt-10  px-2 '>
+        <div className='p-2  md:flex-row md:items-start  md:gap-x-3 justify-center items-center gap-y-5  mt-10  px-2 '>
 
             {/* sidebar */}
-            <div className=' font-DanaDemiBold flex sm:flex-col justify-around mx-auto items-center sm:w-[260px] w-full  h-fit px-2 py-2 rounded-lg  shadow-dark text-sm'>
-                <CgProfile className='hidden md:block text-5xl text-gray-600' />
-                <p className='hidden md:block p-1 my-2 text-gray-600 font-semibold border-b-2 border-b-gray-500'>
+            <div className=' font-DanaDemiBold mb-10 flex justify-around mx-auto items-center  w-full  h-fit px-2 py-2 rounded-lg  shadow-dark text-sm'>
+                <div className='hidden  sm:flex items-center gap-x-1'>
+                <CgProfile className=' text-4xl  text-gray-600' />
+                <p className='   text-gray-600 font-semibold '>
                     {session?.user.email}
                 </p>
+                </div>
 
                 <Link
                     href="/dashboard"
-                    className='text-blue-600 my-2 hover:text-blue-700 transition-all ease-in-out'>
+                    className='text-blue-600  hover:text-blue-700 transition-all ease-in-out'>
                     حساب کاربری
                 </Link>
                 <Link
                     href="/dashboard/my-profiles"
-                    className='text-blue-600 my-2 hover:text-blue-700 transition-all ease-in-out'>
+                    className='text-blue-600  hover:text-blue-700 transition-all ease-in-out'>
                     آگهی های من
                 </Link>
                 <Link
                     href="/dashboard/add"
-                    className='text-blue-600 my-2 hover:text-blue-700 transition-all ease-in-out'>
+                    className='text-blue-600  hover:text-blue-700 transition-all ease-in-out'>
                     ثبت آگهی
                 </Link>
                 <div> <LogOutButton/></div>
