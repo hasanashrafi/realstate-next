8d0edf3393efd5b6722b796bfa094d4cafdd0d4a
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-08 17:46:12 +0330

    create edit api route for edit profiles

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -32,7 +32,7 @@ export async function POST(req) {
         }
 
         const user = await User.findOne({ email: session.user.email })
-        
+
         if (!user) {
             return NextResponse.json(
                 { error: "حساب کاربری وجود ندارد." },
@@ -82,3 +82,88 @@ export async function POST(req) {
         )
     }
 }
+
+export async function PATCH(req) {
+    try {
+        await connectDB();
+
+        const {
+            _id,
+            title,
+            description,
+            location,
+            phone,
+            price,
+            realEstate,
+            constructionDate,
+            category,
+            rules,
+            amenities } = await req.json();
+
+        const session = await getServerSession(req)
+        if (!session) {
+            return NextResponse.json(
+                { error: "وارد حساب کاربری شوید." },
+                { status: 401 }
+            )
+        }
+
+        const user = await User.findOne({ email: session.user.email })
+        if (!user) {
+            return NextResponse.json(
+                { error: "حساب کاربری وجود ندارد." },
+                { status: 404 }
+            )
+        }
+        if (
+            !_id ||
+            !title ||
+            !description ||
+            !location ||
+            !phone ||
+            !price ||
+            !realEstate ||
+            !constructionDate ||
+            !category
+        ) {
+            return NextResponse.json(
+                { error: "اطلاعات آگهی را وارد نمایید." },
+                { status: 400 }
+            )
+        }
+
+
+        const profile = await Profile.findOne({ _id })
+        if (!user._id.equals(profile.userId)) {
+            return NextResponse.json(
+                { error: "دسترسی شما به این آگهی محدود شده است." },
+                { status: 403 }
+            )
+        }
+
+        profile.title = title
+        profile.description = description
+        profile.location = location
+        profile.phone = phone
+        profile.price = price
+        profile.realEstate = realEstate
+        profile.constructionDate = constructionDate
+        profile.category = category
+        profile.amenities = amenities
+        profile.rules = rules
+        profile.save()
+
+        return NextResponse.json(
+            { message: "آگهی با موفقیت ویرایش شد." },
+            { status: 201 }
+        )
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            { error: "مشکلی در ارتباط با سرور رخ داده است" },
+            { status: 500 }
+        )
+    }
+}
+
