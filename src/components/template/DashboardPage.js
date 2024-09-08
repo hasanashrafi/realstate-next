d58c2562e5eb02b3f5d0089fffc6c91a76a256d3
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-08 17:47:36 +0330

    edit styles

--- src/components/template/DashboardPage.js
+++ src/components/template/DashboardPage.js
@@ -6,7 +6,7 @@ function DashboardPage({ createdAt }) {
     const persianDate = moment(createdAt).format('jYYYY/jMM/jDD');
 
     return (
-        <div className='p-6  font-DanaDemiBold min-h-screen'>
+        <div className=' p-6  font-DanaDemiBold min-h-screen'>
             <p className='text-lg text-blue-600 p-2 mb-5'>سلام 👋</p>
             <p className='text-lg text-red-600'>آگهی های خود را ثبت کنید تا هزاران نفر آن را ببینند.</p>
             <div className='p-2 w-72 bg-violet-200 rounded-md my-10'>
