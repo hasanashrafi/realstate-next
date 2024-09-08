1d3f0983fee0aa907bb9c6dfdf282e37e6db4038
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-08 17:45:18 +0330

    added bg

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className="">
+      <body className="bg-[url('/images/bg-2.jpg')] bg-cover backdrop-blur-sm">
         <NexAuthProvider>
           <Layout>
             {children}
