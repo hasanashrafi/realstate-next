6707a6880b6f49f71b2c2d1034bdb62840f90fc5
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-09 22:22:24 +0330

    added div tag

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,9 +6,7 @@ function Layout({ children }) {
     return (
         <>
             <Header />
-            <div>
                 {children}
-            </div>
             <Footer />
         </>
     )
