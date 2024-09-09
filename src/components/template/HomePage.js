491d5a7745c6a0999e7bfeaeaa95fe91b5f81c01
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-09 22:25:53 +0330

    create home page component

---
+++ src/components/template/HomePage.js
@@ -0,0 +1,143 @@
+import Image from 'next/image';
+import React from 'react'
+import { CiCircleCheck } from "react-icons/ci";
+import { FaHouseCircleCheck } from "react-icons/fa6";
+import CategoryCard from '../module/CategoryCard';
+
+function HomePage() {
+    const services = [
+        "خرید",
+        "رهن",
+        "اجاره",
+        "فروش",
+    ]
+    const cities = [
+        "تهران",
+        "تبریز",
+        "شیراز",
+        "اصفهان",
+        "کرج",
+        "مشهد",
+        "رشت",
+        "بابلسر"
+    ]
+
+    return (
+        <div className='font-Dana max-w-5xl my-10 w-[95%] mx-auto min-h-screen  rounded-md  p-4'>
+
+            <div className='p-2 '>
+                <p className='text-4xl text-center my-5 font-DanaDemiBold text-white'>
+                    سامانه خرید و اجاره ملک
+                </p>
+                <p className='text-center text-sky-300 text-xl'>شرکت آلفا ملک</p>
+                <ul className='flex my-4 gap-x-7 mx-auto justify-around text-white  p-2'>
+                    {services.map((i) => (
+                        <li key={i}
+                            className='flex items-center justify-center w-20 hover:w-24  p-2 border-b-2 border-b-white cursor-pointer transition-all ease-in-out hover:border-b-gray-300 '
+                        >
+                            <span> <FaHouseCircleCheck className='text-xl  ' /></span>
+                            <span className='block  mx-2'> {i} </span>
+
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {/* image */}
+            <div className='w-full flex flex-wrap my-4 mx-auto justify-center gap-2'>
+                <div className='relative'>
+                    <Image
+                        priority
+                        alt='bg'
+                        width={400}
+                        height={200}
+                        src='/images/bg-2.jpg'
+                        className=' w-[300px] h-fit rounded hover:scale-105 '
+                    >
+
+                    </Image>
+                    <span className='p-2  absolute bottom-0 left-0 text-xl  text-white'>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ                    </span>
+                </div>
+                <div className='relative'>
+                    <Image
+                        priority
+                        alt='bg'
+                        width={400}
+                        height={200}
+                        src='/images/bg-2.jpg'
+                        className=' w-[300px] h-fit rounded hover:scale-105 '
+                    >
+
+                    </Image>
+                    <span className='p-2  absolute bottom-0 left-0 text-xl  text-white'>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ                    </span>
+                </div>
+                <div className='relative'>
+                    <Image
+                        priority
+                        alt='bg'
+                        width={400}
+                        height={200}
+                        src='/images/bg-2.jpg'
+                        className=' w-[300px] h-fit rounded hover:scale-105 '
+                    >
+
+                    </Image>
+                    <span className='p-2  absolute bottom-0 left-0 text-xl  text-white'>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ                    </span>
+                </div>
+                <div className='relative'>
+                    <Image
+                        priority
+                        alt='bg'
+                        width={400}
+                        height={200}
+                        src='/images/bg-2.jpg'
+                        className=' w-[300px] h-fit rounded hover:scale-105 '
+                    >
+
+                    </Image>
+                    <span className='p-2  absolute bottom-0 left-0 text-xl  text-white'>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ                    </span>
+                </div>
+                <div className='relative'>
+                    <Image
+                        priority
+                        alt='bg'
+                        width={400}
+                        height={200}
+                        src='/images/bg-2.jpg'
+                        className=' w-[300px] h-fit rounded hover:scale-105 '
+                    >
+
+                    </Image>
+                    <span className='p-2  absolute bottom-0 left-0 text-xl  text-white'>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ                    </span>
+                </div>
+                <div className='relative'>
+                    <Image
+                        priority
+                        alt='bg'
+                        width={400}
+                        height={200}
+                        src='/images/bg-2.jpg'
+                        className=' w-[300px] h-fit rounded hover:scale-105 '
+                    >
+
+                    </Image>
+                    <span className='p-2  absolute bottom-0 left-0 text-xl  text-white'>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ                    </span>
+                </div>
+
+
+            </div>
+            {/* category */}
+            <div className=''>
+                <CategoryCard title="ویلا " name="villa" />
+            </div>
+
+        </div>
+    )
+}
+
+export default HomePage
