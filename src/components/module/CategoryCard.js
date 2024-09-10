37f8b21a5f2b14635cd33cc016659e4df400f129
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-10 19:22:44 +0330

    create category card component

--- src/components/module/CategoryCard.js
+++ src/components/module/CategoryCard.js
@@ -1,13 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-function CategoryCard() {
-  return (
-    <div className=' '>
+function CategoryCard({ title, name }) {
+    return (
+        <div className='w-full rounded-md hover:scale-105 transition-all ease-in-out'>
+            <Link href="/">
+                <Image
+                    src={`/images/${name}.png`}
+                    alt={title}
+                    width={240}
+                    height={144}
+                    priority={true}
+                    className=' rounded'
+                />
+            <p className=' text-center mt-2 text-white '>{title}</p>
+            </Link>
 
-    CategoryCard
-    
-    </div>
-  )
+        </div>
+    )
 }
 
 export default CategoryCard
