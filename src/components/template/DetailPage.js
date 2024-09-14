c8bf39f3fb1849045461c2c606502b071b18b4c8
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-14 13:43:21 +0330

    remove constants

--- src/components/template/DetailPage.js
+++ src/components/template/DetailPage.js
@@ -1,15 +1,15 @@
 import { MdOutlineRealEstateAgent } from "react-icons/md";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { FaHandHoldingUsd } from "react-icons/fa";
-import { GiVillage } from "react-icons/gi";
-import { MdApartment } from "react-icons/md";
-import { RiHomeOfficeFill } from "react-icons/ri";
-import { FaStoreAlt } from "react-icons/fa";
-import { FaArrowLeftLong } from "react-icons/fa6";
 import { MdOutlinePhoneInTalk } from "react-icons/md";
+import { BsCalendar2Date } from "react-icons/bs";
+
 
 import ItemList from "../module/ItemList";
 import { sp } from "@/utils/replaceNumbers";
+import ShareButton from "../module/ShareButton";
+import {icons} from "@/constants/icons";
+import { categories } from "@/constants/strings";
 
 
 
@@ -25,39 +25,33 @@ function DetailPage({ data: {
     constructionDate,
     price
 } }) {
-    const categories = {
-        apartment: "آپارتمان",
-        villa: "ویلا",
-        store: "مغازه",
-        office: "دفتر",
-    }
+   
 
-    const icons = {
-        villa: <GiVillage />,
-        apartment: <MdApartment />,
-        office: <RiHomeOfficeFill />,
-        store: <FaStoreAlt />,
-    }
+  
 
     return (
-        <div className='flex justify-between'>
+        <div className='flex justify-between  max-w-7xl mx-auto bg-backdrop-blur-xl border-white border rounded-md'>
             {/* main */}
-            <div className='flex flex-col gap-y-4 p-4'>
-                <p className="">{title}</p>
-                <span className=" ">
-                    <p className="flex items-center gap-x-2"> <FaMapLocationDot />آدرس</p>
+            <div className='w-full flex flex-col gap-y-4 p-4'>
+                <p className="text-2xl font-DanaDemiBold">
+                    {title}
+                </p>
+
+                <p className="flex items-center gap-x-2">
+                    <FaMapLocationDot />
                     {location}
-                </span>
-                <h3>
+                </p>
+
+                <h3 className="mt-4 border-b-2 p-2  w-full">
                     توضیحات:
                 </h3>
                 <p>{description}</p>
-                <h3>
+                <h3 className="border-b-2 p-2  w-full">
                     امکانات:
                 </h3>
                 <ItemList data={amenities} />
 
-                <h3>
+                <h3 className="border-b-2 p-2  w-full">
                     قوانین:
                 </h3>
                 <ItemList data={rules} />
@@ -65,9 +59,9 @@ function DetailPage({ data: {
             </div>
 
             {/* left sideBar */}
-            <div className=' h-fit p-4 rounded'>
-                <div className="flex flex-col items-center gap-y-4">
-                    <MdOutlineRealEstateAgent className="text-3xl bg-blue-600 rounded-full w-12 h-12 p-2 " />
+            <div className=' w-64 h-fit p-4 rounded'>
+                <div className="flex flex-col items-center p-2 text-blue-600 bg-white rounded-md gap-y-4">
+                    <MdOutlineRealEstateAgent className="text-3xl border border-blue-600 rounded-full w-12 h-12 p-2 " />
                     <p>املاک {realEstate}</p>
                     <span className="flex items-center gap-x-1">
                         <MdOutlinePhoneInTalk className="text-xl" />
@@ -75,16 +69,23 @@ function DetailPage({ data: {
                     </span>
                 </div>
 
-                <div className="my-5">
+                <div className=" flex flex-col p-2 bg-white text-blue-600 rounded-md gap-y-3 my-5">
                     <p className="flex items-center gap-x-2">
                         {icons[category]}
-                       <span className="text-2xl"> {categories[category]}</span>
+                        <span className=""> {categories[category]}</span>
                     </p>
-                    <p className="flex items-center gap-x-2 ">
-                    <FaHandHoldingUsd className="size-6 "/>
-                    {sp(price)}
+                    <p className="flex  gap-x-2 ">
+                        <FaHandHoldingUsd className=" " />
+                        {sp(price)}
                     </p>
+                    <p className="flex  gap-x-2">
+                        <BsCalendar2Date />
+                        {new Date(constructionDate).toLocaleDateString("fa-IR")}
+                    </p>
+
                 </div>
+                <div className="">
+                    <ShareButton /></div>
 
             </div>
         </div>
