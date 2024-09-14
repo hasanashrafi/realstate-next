afd77ba06496662ca7b7f9454911b57b578377a3
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-14 13:43:50 +0330

    create icons constants

---
+++ src/constants/icons.js
@@ -0,0 +1,13 @@
+import { GiVillage } from "react-icons/gi";
+import { MdApartment } from "react-icons/md";
+import { RiHomeOfficeFill } from "react-icons/ri";
+import { FaStoreAlt } from "react-icons/fa";
+
+const icons = {
+    villa: <GiVillage />,
+    apartment: <MdApartment />,
+    office: <RiHomeOfficeFill />,
+    store: <FaStoreAlt />,
+}
+
+export { icons }
