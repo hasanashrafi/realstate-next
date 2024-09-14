3e4c6636cc5e05587dd35593155fc4c2c6e1de34
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-14 13:44:05 +0330

    create string constants

---
+++ src/constants/strings.js
@@ -0,0 +1,26 @@
+const services = [
+    "خرید",
+    "رهن",
+    "اجاره",
+    "فروش",
+]
+const cities = [
+    "تهران",
+    "تبریز",
+    "شیراز",
+    "اصفهان",
+    "کرج",
+    "مشهد",
+    "رشت",
+    "بابلسر",
+    "مرند",
+
+]
+
+const categories = {
+    apartment: "آپارتمان",
+    villa: "ویلا",
+    store: "مغازه",
+    office: "دفتر",
+}
+export { services, cities ,categories}
