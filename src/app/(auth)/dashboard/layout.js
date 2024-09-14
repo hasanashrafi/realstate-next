5c63c30a29c8518b2a475a2f548b205aaee8d3da
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-14 22:11:57 +0330

    get user session

--- src/app/(auth)/dashboard/layout.js
+++ src/app/(auth)/dashboard/layout.js
@@ -4,13 +4,20 @@ import { redirect } from 'next/navigation'
 
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import DashboardSideBar from '@/components/layout/DashboardSideBar'
+import connectDB from '@/utils/connectDB'
+import User from '@/models/User'
 
 async function DashboardLayout({ children }) {
     const session = await getServerSession(authOptions)
     if (!session) redirect("/signin")
 
+    await connectDB()
+    const user = await User.findOne({ email: session.user.email })
+    if (!user) return <p>مشکلی رخ داده است.</p>
+
     return (
-        <DashboardSideBar className='min-h-screen bg-red-500 '>
+        <DashboardSideBar email={user.email} role={user.role}
+            className='min-h-screen bg-red-500 '>
             {children}
         </DashboardSideBar>
     )
