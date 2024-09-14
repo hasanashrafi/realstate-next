68490f1d3530fba7d742a7fd2bb04c27c28fa6b2
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-14 22:13:22 +0330

    show link after user role

--- src/components/layout/DashboardSideBar.js
+++ src/components/layout/DashboardSideBar.js
@@ -1,24 +1,22 @@
 import Link from 'next/link'
-import { getServerSession } from 'next-auth'
 
 
 import { CgProfile } from "react-icons/cg"
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import LogOutButton from '../module/LogOutButton'
 
 
-async function DashboardSideBar({ children }) {
-    const session = await getServerSession(authOptions)
+async function DashboardSideBar({ children, email, role }) {
 
     return (
         <div className='  p-2  md:flex-row md:items-start  md:gap-x-3 justify-center items-center gap-y-5    px-2 '>
             {/* sidebar */}
             <div className='font-DanaDemiBold mb-10 flex justify-around mx-auto items-center  w-full  h-fit px-2 py-2 rounded-lg  text-sm'>
                 <div className='hidden  sm:flex items-center gap-x-1'>
-                <CgProfile className=' text-4xl  text-gray-200' />
-                <p className='   text-gray-200  '>
-                    {session?.user.email}
-                </p>
+                    <CgProfile className=' text-4xl  text-gray-200' />
+                   
+                    <p className='   text-gray-200  '>
+                        {email}
+                    </p>
                 </div>
 
                 <Link
@@ -36,8 +34,17 @@ async function DashboardSideBar({ children }) {
                     className='text-blue-100  hover:text-blue-500 transition-all ease-in-out'>
                     ثبت آگهی
                 </Link>
-                <div> <LogOutButton/></div>
-               
+                {
+                    role === "ADMIN" ? (
+                        <Link
+                            className='text-blue-300  hover:text-blue-500 transition-all ease-in-out'
+                            href="/">
+                            در انتظار تایید
+                        </Link>
+                    ) : null
+                }
+                <div> <LogOutButton /></div>
+
             </div>
             {/* main */}
             <div className='w-[95%] mx-auto'>
