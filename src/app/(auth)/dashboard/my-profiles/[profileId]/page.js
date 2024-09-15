5433d90509ca423156fa028a100d65ddb2bdbd66
Hasan Ashrafi <yasashrafi@gmail.com>
2024-09-15 18:03:36 +0330

    added meta tags

--- src/app/(auth)/dashboard/my-profiles/[profileId]/page.js
+++ src/app/(auth)/dashboard/my-profiles/[profileId]/page.js
@@ -6,10 +6,22 @@ import React from 'react'
 async function EditPage({ params: { profileId } }) {
     await connectDB()
     const profile = await Profile.findOne({ _id: profileId })
-    if(!profile) return <p>مشکلی پیش آمده است لطفا دوباره امتحان کنید</p>
-  
-  
+    if (!profile) return <p>مشکلی پیش آمده است لطفا دوباره امتحان کنید</p>
+
+
     return <AddProfilePage data={JSON.parse(JSON.stringify(profile))} />
 }
 
 export default EditPage
+
+
+export const generateMetadata = async ({ params: { profileId } }) => {
+    await connectDB()
+    const profile = await Profile.findOne({ _id: profileId })
+
+    return {
+        title: profile.title,
+        description: profile.description,
+        authors: { name: profile.realEstate },
+    }
+}
